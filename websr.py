ccd2eae7c81c70805a32c0f4d20a5ac55d823c2f
bsubbaraman <bms12014@mymail.pomona.edu>
2021-01-06 19:18:18 -0800

    progress
    send post data, change up ~aesthetic~

--- websr.py
+++ websr.py
@@ -11,28 +11,60 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.tokenize import sent_tokenize
      
 app = Flask(__name__) 
-     
+THE_DATA = []
+
 @app.route('/') 
 def index(): 
-    sources = [Path(t).stem.replace('.sentiment','') for t in glob.glob('./websr/data/*.csv')] #["bluets", "foucault"] are default; plus any added by user
+    sources = [Path(t).stem.replace('.sentiment','') for t in glob.glob('./data/*.csv')] #["bluets", "foucault"] are default; plus any added by user
     print(f"my source materials are:{sources}")
     return render_template('index.html', sources=sources) 
+
+@app.route('/post/', methods = ["POST"])
+def post():
+    print(request.get_data().decode())
+    return 'sup'
      
-@app.route('/square/', methods=['POST']) 
-def square(): 
+@app.route('/run/', methods=['POST']) 
+def run(): 
     val = str(request.form.get('number', 0))
-    # val = str(request.form.get('number'))
     source = str(request.form.get('source', 0))
+    txt_format = str(request.form.get('format', 0))
+    curve = int(request.form.get('curve', 1))
     print(f'my source is {source}')
     if source == "NewFile":
         fpath = str(request.form.get('filepath', 0))
         fpath = fpath.split('\\')[-1]
         print(fpath)
         get_sentiments(f'./data/{fpath}')
-    square = get_log(val, source, 1)
-    data = {'square': square} 
+        return jsonify({'text': f'got the sentiments from {fpath}'})
+    txt = get_log(val, source, curve, txt_format)
+    data = {'text': txt} 
     data = jsonify(data) 
     return data 
+
+@app.route('/realtime/', methods=['POST']) 
+def realtime(source, curve):
+    outfile = time.strftime('%Y%m%d-%H%M%S', time.localtime()) + f"_{source}.txt"
+    raw_outfile = "./logs/raw/" + outfile
+    text_outfile = "./logs/text/" + outfile
+    f1 = open(raw_outfile, 'a')
+    f2 = open(text_outfile, 'a')
+    df = pd.read_csv(f'./data/{source}.sentiment.csv', delimiter=',')
+    sentence_data = [list(row) for row in df.values]
+    sentiments = [float(x[1]) for x in sentence_data]
+    while True:
+        try:
+            ser_bytes = ser.readline()
+            decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+            data = remap(decoded_bytes, 75, 85, -1, 1, curve) #input range will vary on biodata(/person/etc). output should be [-1,1] for compound sentiment 
+            sentence = biodata_to_text(data, sentence_data, sentiments)
+            f1.write(str(decoded_bytes)+'\n')
+            f2.write(sentence+'\n\n')
+        except KeyboardInterrupt:
+            f1.close()
+            f2.close()
+            print(f'data saved to {raw_outfile} and {text_outfile}')
+            exit(0)
 
 @app.route('/format/', methods=['POST']) 
 def format_text(): 
@@ -46,7 +78,7 @@ def format_text():
         new_txt = txt.replace('<br>', "  ")
     elif format_type == "log":
         new_txt = txt.replace("  ", "<br>")
-    data = {'square': new_txt} 
+    data = {'text': new_txt} 
     data = jsonify(data) 
     return data 
 
@@ -74,7 +106,7 @@ def get_sentiments(file_path):
 
     return outfile
 
-def get_log(raw_file, source, curve):
+def get_log(raw_file, source, curve, txt_format):
     df = pd.read_csv(f'./data/{source}.sentiment.csv', delimiter=',')
     sentence_data = [list(row) for row in df.values]
     sentiments = [float(x[1]) for x in sentence_data]
@@ -84,16 +116,16 @@ def get_log(raw_file, source, curve):
         if data:
             sentence_match = biodata_to_text(remap(float(data), 75, 85, -1, 1, curve), sentence_data, sentiments)
             out += sentence_match 
-            out += '<br/>'
-
-
+            if txt_format == 'log':
+                out += '<br>'
+            elif txt_format == 'prose':
+                out += '  '
     return out
 
 
 def biodata_to_text(data, sentence_data, sentiments):  
     sentiment_match = min(sentiments, key = lambda x: abs(x-data))
     sentence_match = sentence_data[sentiments.index(sentiment_match)]
-    print(sentence_match)
     return sentence_match[0]
  
 def remap(value, min_i, max_i, min_f, max_f, curve):
@@ -116,4 +148,4 @@ def remap(value, min_i, max_i, min_f, max_f, curve):
         return new_val
 
 if __name__ == '__main__': 
-    	app.run(debug=True) 
+    	app.run(host='0.0.0.0', debug=True) 
